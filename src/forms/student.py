17eb43557f9dcfbd871aa8fbb39690c83b0efc64
ZeynX92 <sasha.sh.1980@mail.ru>
2023-11-02 22:33:28 +0300

    __validate-number-and-email__

--- src/forms/student.py
+++ src/forms/student.py
@@ -1,3 +1,5 @@
+import re
+import phonenumbers
 from PyQt5 import uic, QtWidgets
 from data import db_session
 from data.students import Student
@@ -22,6 +24,25 @@ class AddStudentForm(QWidget):
 
         self.delete_2.setVisible(False)
 
+    @staticmethod
+    def validate_phone(number):
+        """Проверяет действительность телефонного номера.
+
+        Проверка и анализ номеров осуществляется с помощью библиотеки Phonenumbers Python.
+        """
+        try:
+            parsed_phone = phonenumbers.parse(number)
+            bool_valid_phone = phonenumbers.is_valid_number(parsed_phone)
+        except phonenumbers.NumberParseException as e:
+            bool_valid_phone = False
+        return bool_valid_phone
+
+    @staticmethod
+    def check_email(email):
+        """Проверяет правильность написания адреса электронной почты."""
+        bool_valid_email = re.match(r'[a-zA-Z0-9][a-zA-Z0-9_.-]{1,63}?@[a-zA-Z0-9_.-]{2,63}\.[a-zA-Z]+', email)
+        return bool_valid_email
+
     def save_pr(self):
         surname = self.lineEdit.text()
         name = self.lineEdit_2.text()
@@ -35,6 +56,12 @@ class AddStudentForm(QWidget):
         if db_sess.query(Student).filter(
                 and_(Student.surname == surname, Student.name == name, Student.lastname == lastname)).first():
             self.warn = Warn("Такой ученик уже есть")
+            self.warn.show()
+        elif not self.validate_phone(telephone_number) and telephone_number:
+            self.warn = Warn("Неверный формат телефона")
+            self.warn.show()
+        elif not self.check_email(email) and email:
+            self.warn = Warn("Неверный формат E-mail")
             self.warn.show()
         else:
 
